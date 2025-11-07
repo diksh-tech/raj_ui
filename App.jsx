d8dba14817a98bb1d6f8a8ba774f2c5538dda4b1
diksh-tech <srivastavadeeksha76@gmail.com>
2025-11-07 15:35:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatPage from "./components/ChatPage.jsx";
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <ChatPage />
+    </div>
+  );
+}
